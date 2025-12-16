614754729d7a69cf7456f7f08004d25f7524f839
ankitakhot-15 <ankitakhot012@gmail.com>
2025-12-16 20:44:48 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,17 @@
 // export default defineConfig({
 //   plugins: [react()],
 // })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/ankita-khot-portfolio",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // '@' maps to '/src'
+      "@": path.resolve(__dirname, "src"), // '@' maps to '/src'
     },
   },
   server: {
